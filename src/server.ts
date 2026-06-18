903b82b7503d40719fe13e837b873faa8a3c70ff
Dipesh <dipeshchourasiya12@gmail.com>
2026-06-18 22:03:15 +0530

    set up completed

---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import http from "http";
+
+import app from "./app";
+
+import { connectDB } from "./config/db";
+
+import { env } from "./config/env";
+
+const startServer = async () => {
+  await connectDB();
+
+  const server = http.createServer(app);
+
+  server.listen(env.PORT, () => {
+    console.log(`Server running on port ${env.PORT}`);
+  });
+};
+
+startServer();
